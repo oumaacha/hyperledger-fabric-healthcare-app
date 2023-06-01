118287268f456b859ce93e6cb841b0d8f072079e
ilham Ab <ilhamab@MacBook-Air.local>
2023-06-01 13:06:24 +0100

    Adding Patient dashboard

--- securehealth-app/src/App.jsx
+++ securehealth-app/src/App.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from './Pages/Admin/AdminDash';
+import Patient from './Pages/Patient/PatientDash'
+import Doctor from './Pages/Doctor/DoctorDash'
 import Login from './Pages/LoginPage/Login';
-import AppRoutes from "./Components/Admin/AppRoutes";
+import AppRoutesAdmin from "./Components/Admin/AppRoutes";
+import AppRoutesPatient from "./Components/Patient/AppRoutes"
+import AppRoutesDoctor from "./Components/Doctor/AppRoutes";
 
 const App = () => {
   return (
@@ -11,8 +15,14 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/admin/*" element={<Admin />}>
-            <Route index element={<AppRoutes />} />
-                </Route>
+            <Route index element={<AppRoutesAdmin />} />
+        </Route>
+        <Route path="/patient/*" element={<Patient />}>
+            <Route index element={<AppRoutesPatient />} />
+        </Route>
+        <Route path="/doctor/*" element={<Doctor />}>
+            <Route index element={<AppRoutesDoctor />} />
+        </Route>
         
       </Routes>
     </BrowserRouter>
