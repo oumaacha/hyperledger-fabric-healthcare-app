45b18b3dd2a4f95d4938e2b3f210e9f7d28dfd72
oumaacha <mr.anouar.oumaacha@gmail.com>
2023-06-03 17:14:04 +0100

    Changing patient-contract to smart-contract and add some methods

--- chaincode/securehealth_chaincode/go/entities/Patient.go
+++ chaincode/securehealth_chaincode/go/entities/Patient.go
@@ -1,21 +1,25 @@
 package entities
 
 type Patient struct {
-	PatientID        string `json:"patientId"`
-	FirstName        string `json:"firstName"`
-	LastName         string `json:"lastName"`
-	Age              int	`json:"age"`
-	Password         string `json:"password"`
-	IsDeleted	 bool   `json:"isDeleted"`
+	PatientID         string   `json:"patientId"`
+	FirstName         string   `json:"firstName"`
+	LastName          string   `json:"lastName"`
+	Age               int	  `json:"age"`
+	Password          string   `json:"password"`
+	PermissionGranted []string `json:"permissionGranted"`
+	ChangedBy	  string   `jsos:"changedBy"`
+	IsDeleted	  bool     `json:"isDeleted"`
 }
 
-func NewPatient(patientID string , firstName string, lastName string, password string, age int ) *Patient {
+func NewPatient(patientID string , firstName string, lastName string, age int, password string, changedBy string ) *Patient {
 	return &Patient{
-		PatientID:        patientID,
-		FirstName:        firstName,
-		LastName:         lastName,
-		Age:              age,
-		Password:	  password,
-		IsDeleted:	  false,
+		PatientID:         patientID,
+		FirstName:         firstName,
+		LastName:          lastName,
+		Age:               age,
+		Password:	   password,
+		PermissionGranted: []string{},
+		ChangedBy:	   changedBy,
+		IsDeleted:	   false,
 	}
 }
--- chaincode/securehealth_chaincode/go/smart-contract.go
+++ chaincode/securehealth_chaincode/go/smart-contract.go
@@ -22,11 +22,11 @@ type QueryResult struct {
 func (c *PrimaryContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
 	fmt.Println("============= START : Initialize Ledger ===========")
 	initPatients := []entities.Patient{
-		{PatientID: "PT0", FirstName: "Anouar1", LastName: "Oumaacha1", Age: 24, Password: "mypass1"},
-		{PatientID: "PT1", FirstName: "Anouar2", LastName: "Oumaacha2", Age: 23, Password: "mypass2"},
-		{PatientID: "PT2", FirstName: "Anouar3", LastName: "Oumaacha3", Age: 23, Password: "mypass3"},
-		{PatientID: "PT3", FirstName: "Anouar4", LastName: "Oumaacha4", Age: 18, Password: "mypass4"},
-		{PatientID: "PT4", FirstName: "Anouar5", LastName: "Oumaacha5", Age: 28, Password: "mypass5"},
+		{PatientID: "PAT0", FirstName: "Anouar1", LastName: "Oumaacha1", Age: 24, Password: "mypass1"},
+		{PatientID: "PAT1", FirstName: "Anouar2", LastName: "Oumaacha2", Age: 23, Password: "mypass2"},
+		{PatientID: "PAT2", FirstName: "Anouar3", LastName: "Oumaacha3", Age: 23, Password: "mypass3"},
+		{PatientID: "PAT3", FirstName: "Anouar4", LastName: "Oumaacha4", Age: 18, Password: "mypass4"},
+		{PatientID: "PAT4", FirstName: "Anouar5", LastName: "Oumaacha5", Age: 28, Password: "mypass5"},
 	}
 
 	for i, patient := range initPatients {
@@ -44,6 +44,41 @@ func (c *PrimaryContract) InitLedger(ctx contractapi.TransactionContextInterface
 	}
 
 	fmt.Println("============= END : Initialize Ledger ===========")
+	return nil
+}
+
+func (c *PrimaryContract) grantAccessToDoctor(ctx contractapi.TransactionContextInterface, patientId string, doctorId string) error {
+	exists, err := c.PatientExists(ctx, patientId)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return errors.New("The patient " + patientId + " does not exist")
+	}
+	patientBytes, err := ctx.GetStub().GetState(patientId)
+	if err != nil {
+		return err
+	}
+
+	patient := new(entities.Patient)
+	err = json.Unmarshal(patientBytes, patient)
+	if err != nil {
+		return err
+	}
+
+	patient.PermissionGranted = append(patient.PermissionGranted, doctorId)
+	patient.ChangedBy = patientId
+
+	patientBytes, err = json.Marshal(patient)
+	if err != nil {
+		return err
+	}
+
+	err = ctx.GetStub().PutState(patientId, patientBytes)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -207,6 +242,75 @@ func (c *PrimaryContract) SetPatientAsDeleted(ctx contractapi.TransactionContext
         return nil
 }
 
+func (c *PrimaryContract) CreatePatient(ctx contractapi.TransactionContextInterface, patientID string, firstName string, lastName string, age int, password string) error {
+    exists, err := c.PatientExists(ctx, patientID)
+    if err != nil {
+      return err
+    }
+    if exists {
+      return fmt.Errorf("the patient %s already exists", patientID)
+    }
+
+    patient := entities.Patient{
+		PatientID:        patientID,
+		FirstName:        firstName,
+		LastName:         lastName,
+		Age:              age,
+		Password:	  password,
+    }
+    patientJSON, err := json.Marshal(patient)
+    if err != nil {
+      return err
+    }
+
+    return ctx.GetStub().PutState(patientID, patientJSON)
+}
+
+func (c *PrimaryContract) GetLatestPatientID(ctx contractapi.TransactionContextInterface) (string, error) {
+    allResults, err := c.QueryAllPatients(ctx)
+    if err != nil {
+        return "", err
+    }
+
+    if len(allResults) == 0 {
+        return "", errors.New("No patients found")
+    }
+
+    latestPatient := allResults[len(allResults)-1]
+    return latestPatient.Record.PatientID, nil
+}
+
+func (c *PrimaryContract) QueryPatientsByLastName(ctx contractapi.TransactionContextInterface, lastName string) ([]*entities.Patient, error) {
+    queryString := map[string]interface{}{
+        "selector": map[string]interface{}{
+            "docType": "patient",
+            "lastName": lastName,
+        },
+    }
+    queryStringStr, _ := json.Marshal(queryString)
+    queryResult, err := ctx.GetStub().GetQueryResult(string(queryStringStr))
+    if err != nil {
+        return nil, err
+    }
+
+    patients := []*entities.Patient{}
+    for queryResult.HasNext() {
+        queryResponse, err := queryResult.Next()
+        if err != nil {
+            return nil, err
+        }
+
+        patient := new(entities.Patient)
+        err = json.Unmarshal(queryResponse.Value, patient)
+        if err != nil {
+            return nil, err
+        }
+
+        patients = append(patients, patient)
+    }
+
+    return patients, nil
+}
 
 func main() {
 
